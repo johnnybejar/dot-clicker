f4d71c4281cc2d9000e409c3b27c58fd4847a7b6
johnny <jbejar2001@yahoo.com>
2023-05-24 03:21:32 -0400

    dots can be placed on the web page

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+
+interface Dot {
+  x: number;
+  y: number;
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [dots, setDots] = useState<Dot[]>([]);
+
+  const placeDot = (e: React.MouseEvent) => {
+    // The diameter of each dot is 25 pixels, so we subtract the radius
+    // from the coordinates so that the dot is centered
+    const dot: Dot = { x: e.clientX - 12.5, y: e.clientY - 12.5 };
+    setDots([...dots, dot]);
+  };
+
+  const resetDots = () => {
+    setDots([]);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div
+        className="canvas"
+        onClick={(e) => {
+          placeDot(e);
+        }}
+      >
+        <>
+          {dots.map((dot: Dot) => {
+            console.log("start");
+            console.log(`placing dot at: (${dot.x}, ${dot.y})`);
+            return (
+              <div
+                className="dot"
+                style={{ left: `${dot.x}px`, top: `${dot.y}px` }}
+              ></div>
+            );
+          })}
+        </>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <button className="reset" onClick={resetDots}>
+        Reset
+      </button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
