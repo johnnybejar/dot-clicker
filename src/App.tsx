93f062efd894d19d641a9b91b4d4a1d7948841a3
johnny <jbejar2001@yahoo.com>
2023-05-30 08:58:59 -0400

    Added undo and redo functions + styling for buttons/color-picker

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Color from "./Color";
 import "./App.css";
 
@@ -10,12 +10,15 @@ interface Dot {
 function App() {
   const [dots, setDots] = useState<Dot[]>([]);
   const [color, setColor] = useState<string>("green");
+  const [redos, setRedos] = useState<Dot[]>([]);
 
   const placeDot = (e: React.MouseEvent) => {
     // The diameter of each dot is 25 pixels, so we subtract the radius
     // from the coordinates so that the dot is centered
     const dot: Dot = { x: e.clientX - 12.5, y: e.clientY - 12.5 };
     setDots([...dots, dot]);
+    // Each time a dot is placed, the redos are wiped
+    setRedos([]);
   };
 
   const removeDot = (index: number) => {
@@ -24,8 +27,27 @@ function App() {
     setDots(dotsList);
   };
 
+  const undoDot = () => {
+    const dotsList = [...dots];
+    const redoDot = dotsList.pop();
+    if (redoDot) {
+      setRedos([...redos, redoDot]);
+    }
+    setDots(dotsList);
+  };
+
+  const redoDot = () => {
+    const redosList = [...redos];
+    const dot = redosList.pop();
+    if (dot) {
+      setDots([...dots, dot]);
+    }
+    setRedos(redosList);
+  };
+
   const resetDots = () => {
     setDots([]);
+    setRedos([]);
   };
 
   const changeDotColor = (color: string) => {
@@ -61,9 +83,41 @@ function App() {
           })}
         </>
       </div>
-      <button className="reset" onClick={resetDots}>
-        Reset
-      </button>
+      <ul className="buttons">
+        <li>
+          <button
+            className="button"
+            onClick={(e) => {
+              e.stopPropagation();
+              resetDots();
+            }}
+          >
+            Reset
+          </button>
+        </li>
+        <li>
+          {dots.length === 0 ? (
+            <button disabled className="button" onClick={undoDot}>
+              Undo
+            </button>
+          ) : (
+            <button className="button" onClick={undoDot}>
+              Undo
+            </button>
+          )}
+        </li>
+        <li>
+          {redos.length > 0 ? (
+            <button className="button" onClick={redoDot}>
+              Redo
+            </button>
+          ) : (
+            <button disabled className="button" onClick={redoDot}>
+              Redo
+            </button>
+          )}
+        </li>
+      </ul>
       <Color onChange={changeDotColor} currentColor={color} />
     </>
   );
